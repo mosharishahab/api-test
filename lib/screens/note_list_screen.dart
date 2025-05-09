e745dfb9d3391d6ca78c5bf7cb1b7d8248f36040
shahab m <128097623+mosharishahab@users.noreply.github.com>
2025-05-09 19:23:50 +0330

    AiDev Pro: auto commit from chat

---
+++ lib/screens/note_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoteListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Note List Screen'),
+    );
+  }
+}
